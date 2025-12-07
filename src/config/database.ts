f383917df1f70195cbe6a7ae105a927d731be425
eyobiabebe <eyobabebe721@gmail.com>
2025-12-07 07:58:20 -0800

    sequalizer edit

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,18 +4,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || '',
-  process.env.DB_USER || '',
-  process.env.DB_PASS || '',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    logging: false, // optional: disable SQL logging
-    dialectOptions: {
-    ssl: { rejectUnauthorized: false }, // Required for Render Postgres
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set in .env');
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
-  }
-);
+});
 
 export default sequelize;
