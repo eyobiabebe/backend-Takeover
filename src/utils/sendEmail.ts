5c364207d03d90f60458085597755f0de826515f
eyobiabebe <eyobabebe721@gmail.com>
2025-12-17 04:56:58 -0800

    fixied sending email for verfication

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,4 +1,4 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
 
 interface EmailOptions {
   to: string;
@@ -6,31 +6,20 @@ interface EmailOptions {
   html: string;
 }
 
-export const sendEmails = async ({ to, subject, html }: EmailOptions) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: false, 
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-    await transporter.sendMail({
-      from: process.env.SMTP_USER ,
+export const sendEmail = async ({ to, subject, html }: EmailOptions) => {
+  try {
+    await sgMail.send({
       to,
+      from: process.env.EMAIL_FROM as string, 
       subject,
       html,
     });
 
     console.log("Email sent to:", to);
-  } catch (error) {
-    console.error("sendEmail error:", error);
+  } catch (error: any) {
+    console.error("SendGrid error:", error.response?.body || error);
     throw new Error("Email could not be sent");
   }
 };
